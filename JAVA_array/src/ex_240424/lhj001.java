e15e6f383b166a4f5a8a519e4a00f3d9b253cbe9
LeeHyunJong <vvlddiz@gmail.com>
2024-04-24 17:18:40 +0900

    이현종 제출 2입니다.

---
+++ JAVA_array/src/ex_240424/lhj001.java
@@ -0,0 +1,26 @@
+package ex_240424;
+
+import java.util.Scanner;
+
+public class lhj001 {
+
+	public static void main(String[] args) {
+		Scanner lunch = new Scanner(System.in);
+		String lunchString[] = new String[5];
+		System.out.println("음식을넣으세요");
+		
+		for (int i = 0; i < lunchString.length; i++) {
+			lunchString[i]  = lunch.next();
+			
+		}
+		for (int i = 0; i < lunchString.length; i++) {
+			System.out.println(lunchString[i]);
+	
+			
+		}
+		System.out.println("이제 그만");
+		
+		lunch.close();
+	}
+
+}
